0e7db1578d93e1e49361e8e2779face40c3f04e4
ShadyProgrammer <sp@sp.com>
2016-11-15 19:36:33 +0000

    Auto installer script for vim

--- installers/vim_env.bash
+++ installers/vim_env.bash
@@ -1,1 +1,3 @@
-test
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl https://raw.githubusercontent.com/Rkieltyka/rados/dotfiles/dotfiles/.vimrc --create-dirs -o ~/.vimrc
+vim +PlugInstall +qall
