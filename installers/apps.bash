d99f0100330390d45c0637a561af8474d233ec6b
ShadyProgrammer <sp@sp.com>
2016-11-15 20:32:30 +0000

    Apps installer

---
+++ installers/apps.bash
@@ -0,0 +1,24 @@
+# General development stuff:
+    
+    sudo apt-get -y install vim git tree httpie terminator
+    
+    # VIM - Text editor on steroids
+    # Git - Version control system
+    # tree - Structured alternative to ls command
+    # httpie - Instead of curl for REST API work
+    # Terminator - Terminal emulator with split windows feature
+
+
+# Python development related:
+   
+    sudo pip install flake8 coverage ipython
+    
+    # flake8 - for PEP8 syntax compliance
+    # coverage - for unit testing reports
+    # ipython - Better than default python shell that keeps command history
+
+# Handy stuff in general:
+    sudo apt-get -y install cmatrix redshift
+    
+    # cmatrix - Cool matrix animation
+    # redshift - Screen light dimmer at night
