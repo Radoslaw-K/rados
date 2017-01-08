71d89f9591068865368b086cf53e9ea84058acf8
Radoslaw Kieltyka <Radoslaw-K@users.noreply.github.com>
2017-01-08 21:04:16 +0000

    Update apps.bash

--- installers/apps.bash
+++ installers/apps.bash
@@ -1,33 +1,32 @@
-# General development stuff:
-    
-    sudo apt-get -y install vim git tree httpie terminator silversearcher-ag sqlite3 strace screen inotify-tools
-    
-    # VIM - Text editor on steroids
-    # Git - Version control system
-    # tree - Structured alternative to ls command
-    # httpie - Instead of curl for REST API work
-    # Terminator - Terminal emulator with split windows feature
-    # silversearcher-ag - Search for a keyword in ALL files in given directory
-    # sqlite3 - Simple database system
-    # strace - Follow commands code execution step by step
-    # screen - Run process in background
-    # inotify-tools - Monitor directories for changes
+#!/bin/bash
 
-# Tools that tend to be missing sometimes for building packages from source
-    sudo apt-get -y install bison libtool build-essentials acloclal autotools-dev automake
+install_sqlite3()
+{
+echo "Installing sqlite3 package"
+sudo apt-get install -y sqlite3
+
+echo "Adding custom config for sqlite3 package"
+
+SQLITE_RC="\
+.header on
+.mode column\n"
+
+printf "$SQLITE_RC" > /home/$USER/.sqliterc
+
+exit 0
+}
+
+##################################    MAIN   ###############################################
+
+if [ $USER == "root" ]; then
+    echo "[ERROR] Not allowed to run script as: $USER. Please log in as a different user."
+    exit 0
+    fi
 
 
-# Python development related:
-   
-    sudo pip install flake8 coverage ipython pexpect
-    
-    # flake8 - for PEP8 syntax compliance
-    # coverage - for unit testing reports
-    # ipython - Better than default python shell that keeps command history
-    # pexpect - ipython dependency
+echo "Installation started..."
 
-# Handy stuff in general:
-    sudo apt-get -y install cmatrix redshift
-    
-    # cmatrix - Cool matrix animation
-    # redshift - Screen light dimmer at night
+sudo apt-get update
+
+install_sqlite3
+
