e4d8705f93f0effeeb2cd6a663dc9b75af13e280
Radoslaw Kieltyka <Radoslaw-K@users.noreply.github.com>
2017-01-09 22:42:05 +0000

    Delete vim_env.bash

--- installers/vim_env.bash
+++
@@ -1,3 +0,0 @@
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl https://raw.githubusercontent.com/Rkieltyka/rados/dotfiles/dotfiles/.vimrc --create-dirs -o ~/.vimrc
-vim +PlugInstall +qall
